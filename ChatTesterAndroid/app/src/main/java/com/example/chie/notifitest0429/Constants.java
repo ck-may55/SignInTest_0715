8c6be9ddb2fccc4ed090f68b9568348a683bc3ab
ChieKubota <16rd050@ms.dendai.ac.jp>
2017-07-15 19:36:19 +0900

    Revert "ログインおよびDB登録の実装"
    This reverts commit 27c17ebb22958cf2d7cbf7b344d83730ce34654b.

--- ChatTesterAndroid/app/src/main/java/com/example/chie/notifitest0429/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.example.chie.notifitest0429;
-
-/**
- * Created by satoruishii on 2017/06/17.
- */
-// 全体で共有する定数をセットするためのクラス
-public class Constants {
-    public static String UID = "";
-}
