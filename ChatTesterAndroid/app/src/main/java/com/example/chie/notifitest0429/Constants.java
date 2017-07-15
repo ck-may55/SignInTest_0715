27c17ebb22958cf2d7cbf7b344d83730ce34654b
ChieKubota <16rd050@ms.dendai.ac.jp>
2017-07-15 19:35:17 +0900

    ログインおよびDB登録の実装

---
+++ ChatTesterAndroid/app/src/main/java/com/example/chie/notifitest0429/Constants.java
@@ -0,0 +1,9 @@
+package com.example.chie.notifitest0429;
+
+/**
+ * Created by satoruishii on 2017/06/17.
+ */
+// 全体で共有する定数をセットするためのクラス
+public class Constants {
+    public static String UID = "";
+}
